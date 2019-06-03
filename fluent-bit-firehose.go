945f5bdd39cd0a341ea323c746862377f9383586
Wesley Pettit <wppttt@amazon.com>
2019-06-03 13:59:31 -0700

    Implemented Amazon Kinesis Data Firehose Fluent Bit plugin

---
+++ fluent-bit-firehose.go
@@ -0,0 +1,105 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package main
+
+import (
+	"C"
+	"fmt"
+	"unsafe"
+
+	"github.com/awslabs/amazon-kinesis-firehose-for-fluent-bit/firehose"
+	"github.com/awslabs/amazon-kinesis-firehose-for-fluent-bit/plugins"
+	"github.com/fluent/fluent-bit-go/output"
+)
+import "github.com/sirupsen/logrus"
+
+var (
+	out *firehose.FirehoseOutput
+)
+
+// The "export" comments have syntactic meaning
+// This is how the compiler knows a function should be callable from the C code
+
+//export FLBPluginRegister
+func FLBPluginRegister(ctx unsafe.Pointer) int {
+	return output.FLBPluginRegister(ctx, "firehose", "Amazon Kinesis Data Firehose Fluent Bit Plugin.")
+}
+
+//export FLBPluginInit
+func FLBPluginInit(ctx unsafe.Pointer) int {
+	plugins.SetupLogger()
+
+	deliveryStream := output.FLBPluginConfigKey(ctx, "delivery-stream")
+	logrus.Infof("[firehose] plugin parameter = '%s'\n", deliveryStream)
+	region := output.FLBPluginConfigKey(ctx, "region")
+	logrus.Infof("[firehose] plugin parameter = '%s'\n", region)
+	dataKeys := output.FLBPluginConfigKey(ctx, "data_keys")
+	logrus.Infof("[firehose] plugin parameter = '%s'\n", dataKeys)
+	roleARN := output.FLBPluginConfigKey(ctx, "role_arn")
+	logrus.Infof("[firehose] plugin parameter = '%s'\n", roleARN)
+
+	if deliveryStream == "" || region == "" {
+		return output.FLB_ERROR
+	}
+
+	var err error
+	out, err = firehose.NewFirehoseOutput(region, deliveryStream, dataKeys, roleARN)
+	if err != nil {
+		logrus.Debugf("firehose: Failed to initialize plugin: %v\n", err)
+		return output.FLB_ERROR
+	}
+	return output.FLB_OK
+}
+
+//export FLBPluginFlush
+func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
+	var count int
+	var ret int
+	var record map[interface{}]interface{}
+
+	// Create Fluent Bit decoder
+	dec := output.NewDecoder(data, int(length))
+
+	fluentTag := C.GoString(tag)
+	logrus.Debugf("firehose: Found logs with tag: %s\n", fluentTag)
+
+	for {
+		// Extract Record
+		ret, _, record = output.GetRecord(dec)
+		if ret != 0 {
+			break
+		}
+
+		err := out.AddRecord(record)
+		if err != nil {
+			return output.FLB_ERROR
+		}
+		count++
+	}
+	err := out.Flush()
+	if err != nil {
+		return output.FLB_ERROR
+	}
+	fmt.Printf("Processed %d events with tag %s\n", count, fluentTag)
+
+	return output.FLB_OK
+}
+
+//export FLBPluginExit
+func FLBPluginExit() int {
+	return output.FLB_OK
+}
+
+func main() {
+}
---
+++ plugins/plugins.go
@@ -0,0 +1,121 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Package plugins contains functions that are useful across fluent bit plugins.
+// This package will be imported by the CloudWatch Logs and Kinesis Data Streams plugins.
+package plugins
+
+import (
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+const fluentBitLogLevelEnvVar = "FLB_LOG_LEVEL"
+
+func SetupLogger() {
+	logrus.SetOutput(os.Stdout)
+	switch strings.ToUpper(os.Getenv(fluentBitLogLevelEnvVar)) {
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	case "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "INFO":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	}
+}
+
+// []byte will be base64 encoded when marshaled to JSON, so we must directly cast all []byte to string
+func DecodeMap(record map[interface{}]interface{}) (map[interface{}]interface{}, error) {
+	for k, v := range record {
+		switch t := v.(type) {
+		case []byte:
+			// convert all byte slices to strings
+			record[k] = string(t)
+		case map[interface{}]interface{}:
+			decoded, err := DecodeMap(t)
+			if err != nil {
+				return nil, err
+			}
+			record[k] = decoded
+		case []interface{}:
+			decoded, err := decodeSlice(t)
+			if err != nil {
+				return nil, err
+			}
+			record[k] = decoded
+		}
+	}
+	return record, nil
+}
+
+// DataKeys allows users to specify a list of keys in the record which they want to be sent
+// all others are discarded
+func DataKeys(input string, record map[interface{}]interface{}) map[interface{}]interface{} {
+	input = strings.TrimSpace(input)
+	keys := strings.Split(input, ",")
+
+	for k := range record {
+		var currentKey string
+		switch t := k.(type) {
+		case []byte:
+			currentKey = string(t)
+		case string:
+			currentKey = t
+		default:
+			logrus.Debugf("firehose: Unable to determine type of key %v\n", t)
+			continue
+		}
+
+		if !contains(keys, currentKey) {
+			delete(record, k)
+		}
+	}
+
+	return record
+}
+
+func decodeSlice(record []interface{}) ([]interface{}, error) {
+	for i, v := range record {
+		switch t := v.(type) {
+		case []byte:
+			// convert all byte slices to strings
+			record[i] = string(t)
+		case map[interface{}]interface{}:
+			decoded, err := DecodeMap(t)
+			if err != nil {
+				return nil, err
+			}
+			record[i] = decoded
+		case []interface{}:
+			decoded, err := decodeSlice(t)
+			if err != nil {
+				return nil, err
+			}
+			record[i] = decoded
+		}
+	}
+	return record, nil
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
